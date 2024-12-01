d5c0c30b7a9ccb6d7f06b9620b3c3bc9906a6d78
metesd0 <sdcheats8@gmail.com>
2024-12-01 19:10:31 +0300

    Update auth.hpp

--- auth.hpp
+++ auth.hpp
@@ -3,16 +3,14 @@
 
 #include <string>
 
-// Disk ID'sini alacak fonksiyon
 std::string getDiskID();
 
-// Key doğrulaması yapacak fonksiyon
 bool validateAuthKey(const std::string& authKey, const std::string& diskID);
 
-// Renk ayarlarını yapacak fonksiyon
+
 void setTextColor(bool isValidKey, bool isExpired);
 
-// Sunucuya bağlanacak ve doğrulama işlemini yapacak ana fonksiyon
+
 void authenticate(const std::string& authKey);
 
-#endif // AUTH_HPP
+#endif
